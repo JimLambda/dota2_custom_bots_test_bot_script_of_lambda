60af66561e4f2237fdf4ceee26f8a466f3057a24
SuperJim <526554176@qq.com>
2022-10-30 17:53:14 +0900

    Commit and sync the github changes of README.md

---
+++ hero_selection.lua
@@ -0,0 +1,6 @@
+function Think()
+    SelectHero(1, "npc_dota_hero_skeleton_king")
+    SelectHero(2, "npc_dota_hero_skeleton_king")
+    SelectHero(3, "npc_dota_hero_skeleton_king")
+    SelectHero(4, "npc_dota_hero_skeleton_king")
+end
