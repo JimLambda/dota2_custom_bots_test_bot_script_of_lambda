0aeec02216faf9f266c09b52448ad2b1f09e86a8
SuperJim <526554176@qq.com>
2022-10-30 17:54:18 +0900

    Still learning. Trying to modify item_purchase_generic.lua

--- item_purchase_generic.lua
+++ item_purchase_generic.lua
@@ -1,5 +1,31 @@
--- Implement ItemPurchaseThink() to override decisionmaking around item purchasing.
-function ItemPurchaseThink()
-    
-	return
+
+
+
+
+-- Item purchase lists for npc_dota_hero_skeleton_king.
+local itemPurchaseListSkeletonKing = {
+    "item_boots", "item_crimson_guard", "item_assault", "item_heart",
+    "item_aeon_disk", "item_ultimate_scepter", "item_aghanims_shard",
+    "item_travel_boots", "item_ultimate_scepter_2", "item_sphere",
+    "item_travel_boots_2", "item_moon_shard"
+}
+
+
+
+
+
+function GetNextItemToPurchaseFromItemList(itemList_stringList)
+    for indexOfTheBigItemInItemList, bigItemName in ipairs(itemList_stringList) do
+		
+    end
 end
+
+
+
+
+
+
+
+
+-- Implement ItemPurchaseThink() to override decisionmaking around item purchasing.
+function ItemPurchaseThink() return end
