4dfae08fcb4ee042aad552769d8630b5debdd03e
SuperJim <526554176@qq.com>
2023-05-14 10:00:48 +0900

    Change hero selection to only 2~5 player slot.

--- hero_selection.lua
+++ hero_selection.lua
@@ -1,5 +1,5 @@
 function Think()
-    for playerID = 0, 4, 1 do
+    for playerID = 1, 4, 1 do
         if GetSelectedHeroName(playerID) == "" then
             SelectHero(playerID, "npc_dota_hero_skeleton_king")
         end
